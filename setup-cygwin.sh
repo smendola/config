fa02e1a770b138aa60af87b4e8c8ada486163368
Salvatore Mendola <smendola@phtcorp.com>
2013-07-01 11:27:01 -0400

    bla

--- setup-cygwin.sh
+++ setup-cygwin.sh
@@ -43,7 +43,7 @@ curl '-#' -o ./cygwin-setup.exe 'http://cygwin.com/setup.exe'
 
 # Run the installer, requesting some specific packages we need
 ./cygwin-setup.exe -q -R 'C:\cygwin' -l 'C:\cygwin-pkgs' \
-    -P tar,zip,unzip,p7zip,vi,openssh,ca-certificates,curl,wget,source-highlight,dos2unix,git,git-completion,zsh,terminfo
+    -P tar,zip,unzip,p7zip,vi,openssh,ca-certificates,curl,wget,source-highlight,dos2unix,git,git-completion,zsh,terminfo,xmlstarlet
 
 echo "**************************************************************"
 echo "Cygwin should now be installed."
