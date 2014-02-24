73b078c03770b3c7246fe36079b09b2aea269f50
Salvatore Mendola <smendola@phtcorp.com>
2014-02-24 16:06:52 -0500

    bla

--- bin/drop-dbs.sh
+++ bin/drop-dbs.sh
@@ -10,12 +10,11 @@ host=${1:-localhost}
 for db in AccessControl Logging Template FileStorage StudyDesign ClinicalData DeviceSynch
 do
     echo -n "Dropping $db ..."
-    psql -h${host} -U postgres -c "DROP DATABASE \"$db\""
-    echo ""
+    psql -h ${host} -U postgres -c "DROP DATABASE \"$db\""
     if [[ $create = 1 ]]
     then
         echo -n "Creating $db as empty database, no schema ..."
-        psql -h${host} -U postgres -c "CREATE DATABASE \"$db\""
+        psql -h ${host} -U postgres -c "CREATE DATABASE \"$db\""
         echo ""
     fi
 done
