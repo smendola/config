880d1b5e79501e0300f192c5cd6b31986dc38f26
Salvatore Mendola <smendola@reachire.com>
2022-04-25 10:53:50 -0400

    docker.start

--- .custom.sh
+++ .custom.sh
@@ -495,12 +495,7 @@ function shutdown() {
   wsl.exe --shutdown
 }
 
-# Start Docker daemon automatically when logging in if not running.
-RUNNING=`ps aux | grep dockerd | grep -v grep`
-if [ -z "$RUNNING" ]; then
-    sudo dockerd > /dev/null 2>&1 &
-    disown
-fi
+sudo pkill -0 dockerd || (sudo dockerd 2> /dev/null&)
 
 alias e=micro
 vi () {
