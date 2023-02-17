dc47d4b326d2e9815df51657115f7b3cf13f90eb
Salvatore Mendola <smendola@reachire.com>
2023-02-17 14:59:20 -0500

    x

--- setup-env.sh
+++ setup-env.sh
@@ -1,27 +1,19 @@
 #!/bin/bash -ue
 
-cd $HOME
-
-read -er -p "What branch do you want to fetch? (e.g. xubuntu, wsl): " BRANCH
-
-rm -rf config
-git config --global --add core.autocrlf false
-git clone https://smendola@bitbucket.org/smendola/config.git
-
-rm -rf .config.git
-git init --separate-git-dir .config.git
-rm .git
 function dots() {
     git --git-dir=$HOME/.config.git/ --work-tree=$HOME "$@"
 }
 
-dots remote add origin https://smendola@bitbucket.org/smendola/config.git
-dots fetch origin $BRANCH
-dots checkout --force $BRANCH
+cd $HOME
 
-rm -rf ~/.oh-my-zsh
-git clone https://smendola@bitbucket.org/smendola/.oh-my-zsh.git
+read -er -p "What branch do you want to fetch? (e.g. xubuntu, wsl): " BRANCH
 
+rm -rf config .config.git .oh-my-zsh
+
+git config --global --add core.autocrlf false
+git clone -b $BRANCH https://smendola@github.com/smendola/config.git
+(cd config && tar cf - .) | tar xf -
+mv .git .config.git
 
 sudo apt-get install -y zsh
 chsh $USER -s /usr/bin/zsh
