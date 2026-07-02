9f6e75972e79d240e0f974ac6dc7e77fcdbf822e
Salvatore Mendola <smendola@reachire.com>
2026-07-02 10:57:15 -0400

    lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,38 @@ local config = wezterm.config_builder()
 ------------------------------------------------------------------------
 -- Appearance
 ------------------------------------------------------------------------
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Medium' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 11,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+  tab_bar = {
+    active_tab = {
+      fg_color = '#c0c0c0',
+      bg_color = '#1a1b26' -- bg of Tokyo Night
+    },
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
 
 config.color_scheme = "Tokyo Night"
 -- config.colors = {
