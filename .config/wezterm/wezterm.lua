c3650ad259dafbea20b3f2c0bf14dbb01468c50d
Salvatore Mendola <smendola@reachire.com>
2026-07-03 00:03:39 -0400

    x

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.color_scheme = "Tokyo Night"
 --   background = "#000000",
 -- }
 
-config.font = wezterm.font('JetBrains Mono', { weight = 'ExtraLight' })
+config.font = wezterm.font('JetBrains Mono', { weight = 'Light' })
 
 config.font_size = 10.0
 config.line_height = 1.0
